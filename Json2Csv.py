5600fe18ead701d74e4194e12cf20382561aefeb
Juan Manuel Capistran Fabela -X (jcapistr - SRA OSS Inc at Cisco) <jcapistr@cisco.com>
2020-12-15 13:15:59 -0600

    fix issue, thanks Luiz Polli

--- Json2Csv.py
+++ Json2Csv.py
@@ -2,11 +2,11 @@ import csv, json, sys
 #if you are not using utf-8 files, remove the next line
 #sys.setdefaultencoding("UTF-8") #set the encode to utf8
 #check if you pass the input file and output file
-if sys.argv[0] is not None and sys.argv[1] is not None:
+if sys.argv[1] is not None and sys.argv[2] is not None:
     print("Input: " + sys.argv[1])
-    print("Output: " + sys.argv[1])|
-    fileInput = sys.argv[0]
-    fileOutput = sys.argv[1]
+    print("Output: " + sys.argv[2])
+    fileInput = sys.argv[1]
+    fileOutput = sys.argv[2]
     inputFile = open(fileInput) #open json file
     outputFile = open(fileOutput, 'w') #load csv file
     data = json.load(inputFile) #load json content
